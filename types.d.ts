1e6dcadf66dc0947b1f26c46ae67761025d58e0d
nfour <novus.nfour@gmail.com>
2020-04-25 14:59:10 +1000

    fix types

--- types.d.ts
+++ types.d.ts
@@ -1,9 +1,11 @@
-export type TsEslintRules = (
-    import('eslint/rules/index').ESLintRules
-  & typeof import('@typescript-eslint/eslint-plugin/dist/configs/all.json')['rules']
-);
+export type TsEslintPluginRules = Record<
+  keyof typeof import('@typescript-eslint/eslint-plugin/dist/configs/all.json')['rules'],
+  any
+>;
+
+export type EslintRules = import('eslint/rules/index').ESLintRules;
+export type TsEslintRules = EslintRules & TsEslintPluginRules;
 
 export type TsEslintConfig = {
-  rules?: TsEslintRules
-}
-
+  rules?: TsEslintRules;
+};
